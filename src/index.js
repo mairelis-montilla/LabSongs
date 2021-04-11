7df3184deea2b87a43064c582212fc179cd28dae
mairelis-montilla <mairelismontilla@gmail.com>
2021-04-10 20:48:34 -0500

    refactor fetch (index.js)

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
 const key = '22f48f41f6aeea5f2f3696185f092dd7';
 const artistA = ['TaylorSwift', 'Camilo', 'BrunoMars', 'Shakira', 'SuperJunior'];
-const artist = [];
-const tracks = [];
+const artistFull = [];
 artistA.forEach((elemnt) => {
+  let artist;
   fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${elemnt}&api_key=${key}&format=json`)
     .then((response) => response.json())
     .then((result) => {
       const { name } = result.artist;
-      const images = result.artist.image;
-      artist.push({ name, images });
+      const images = result.artist.image[1];
+
+      artist = {
+        name,
+        images,
+      };
+      return fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${elemnt}&limit=11&api_key=${key}&format=json`);
     })
-    .catch((error) => console.log('error', error));
-});
-artistA.forEach((elemnt) => {
-  fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${elemnt}&limit=11&api_key=${key}&format=json`)
     .then((response) => response.json())
     .then((result) => {
-      const name = result.toptracks['@attr'].artist;
-      const { track } = result.toptracks;
-      tracks.push({ name, track });
+      artist = {
+        ...artist,
+        track: result.toptracks.track,
+      };
+      artistFull.push(artist);
     })
     .catch((error) => console.log('error', error));
 });
-console.log('GETINFO', artist, tracks);
+console.log(artistFull);
